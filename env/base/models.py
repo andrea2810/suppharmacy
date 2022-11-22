0291ebb9bc47021979a66848fad4e97feef9abf9
andrea2810 <andrear.2810@gmail.com>
2022-11-22 12:40:48 -0600

    Add base module for suppharmacy

---
+++ env/base/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class ResPartner(models.Model):
+	
+	active = modesl.BooleanField()
+	name = models.CharField()
+	birth_date = models.DateField()
+	city = models.CharField()
+	country = modesl.CharField()
+	email = models.CharField()
+	is_company = modesl.BooleanField()
+	mobile = fiels.CharField()
+	parent_id = fields.ForeignKey(ResPartner, on_delete=models.CASCADE)
+	phone = fields.CharField()
+	ref = fields.CharField()
+	rfc = fields.CharField()
+	cp = fields.CharField()	
+
