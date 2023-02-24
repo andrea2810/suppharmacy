2eb2e8afb84f78ea97013adc850e9c4f68793afe
Saul de Alba <salba@exinnotech.com>
2023-02-23 19:19:50 -0600

    Se agrega la integracion con la base de datos postgres

--- api-db/api/core/models.py
+++ api-db/api/core/models.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 
+import psycopg2
+
 class Partner:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, **fields):
+        self.id = fields.get('id', 0)
+        self.name = fields.get('name', '')
 
     @staticmethod
     def get():
-        return [Partner(name="Saulson"), Partner(name="Rouz")]
+        res = []
+
+        with psycopg2.connect(dbname='demo', user='postgres', password='admin', host="192.168.32.1") as conn:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                cur.execute("SELECT * FROM partner")
+                res = [Partner(**rec) for rec in cur.fetchall()]
+
+        return res
+
+    def create(self):
+        with psycopg2.connect(dbname='demo', user='postgres', password='admin', host="192.168.32.1") as conn:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                cur.execute("INSERT INTO partner (name) VALUES(%(name)s) RETURNING id", {
+                        'name': self.name,
+                    })
+                self.id = cur.fetchone()['id']
+            conn.commit()
+        
+        return True
--- api-db/api/core/serializers.py
+++ api-db/api/core/serializers.py
@@ -3,7 +3,10 @@ from rest_framework import serializers
 from .models import Partner
 
 class PartnerSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=100, label='Name', required=True)
 
     def create(self, validated_data):
-        return Partner(**validated_data)
+        intance = Partner(**validated_data)
+        intance.create()
+        return intance
