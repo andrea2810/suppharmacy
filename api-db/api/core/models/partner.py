74dc0504cfc584f8c5dbd295297e419e7cc3cc7a
andrea2810 <andrear.2810@gmail.com>
2023-03-04 17:59:48 -0600

    Se añaden todos los campos de la tabla de res_partner

--- api-db/api/core/models/partner.py
+++ api-db/api/core/models/partner.py
@@ -4,9 +4,19 @@ from .base import BaseModel
 
 class Partner(BaseModel):
 
-    _table = 'partner'
+    _table = 'res_partner'
     _fields = {
             'id': 0, # Integer
+            'active': True, #Boolean
             'name': '', # Varchar
-            'last_name': '' # Varchar 
+            'birth_date': '', #Date
+            'city': '', #Varchar
+            'country': '', #Varchar
+            'email': '', #Varchar
+            'is_company': False, #Boolean
+            'mobile': '', #Varchar
+            'phone': '', #Varchar
+            'ref': '', #Varchar
+            'rfc': '', #Varchar
+            'cp': '' #Varchar
         }
--- api-db/api/core/serializers/partner.py
+++ api-db/api/core/serializers/partner.py
@@ -10,5 +10,14 @@ class PartnerSerializer(APISerializer):
 
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=100, label='Name', required=False)
-    last_name = serializers.CharField(max_length=100, label='Last Name',
-        required=False)
+    active = serializers.BooleanField(required=False)
+    birth_date = serializers.DateField(required=False)
+    city = serializers.CharField(max_length=100, required=False)
+    country = serializers.CharField(max_length=100, required=False)
+    email = serializers.CharField(max_length=100, required=False)
+    is_company = serializers.BooleanField(required=False)
+    mobile = serializers.CharField(max_length=15, required=False)
+    phone = serializers.CharField(max_length=15, required=False)
+    ref = serializers.CharField(max_length=100, required=False)
+    rfc = serializers.CharField(max_length=15, required=False)
+    cp = serializers.CharField(max_length=5, required=False)
